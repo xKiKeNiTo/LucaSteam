5f6f7a19d513c2364a726ec47c4fbddaf585b0fc
Kike <kike.verac@gmail.com>
2024-11-19 13:44:14 +0100

    Test2

--- src/menu/test.java
+++ src/menu/test.java
@@ -1,5 +1,23 @@
 package menu;
 
 public class test {
-    System.out.println("HOla");
+	
+	public static void mostrarMenu() {
+        System.out.println(" ");
+        System.out.println(" EMPRESA V2");
+        System.out.println(" ");
+        System.out.println("************************************************");
+        System.out.println("*  Por favor, escoge una opcion: ");
+        System.out.println("*    1.-Dar de alta un empleado");
+        System.out.println("*    2.-Eliminar un empleado");
+        System.out.println("*    3.-Listar los empleados existentes");
+        System.out.println("*    4.-Indicar si existe un empleado concreto");
+        System.out.println("*    5.-Modificar datos de un empleado");
+        System.out.println("*    (OFF) 6.-Incentivar a los directores");        
+        System.out.println("*    7.-Casos de prueba");
+        System.out.println("*    0.-Terminar sesion");
+        System.out.println("************************************************");
+        System.out.println(" ");
+        System.out.print(">> ");
+    }
 }
