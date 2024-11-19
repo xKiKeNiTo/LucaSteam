43d870f5722cfed42867daa28abf68c844b3d04a
Amohmade <mhassouba26@gmail.com>
2024-11-19 14:16:36 +0100

    Test

--- src/menu/test.java
+++ src/menu/test.java
@@ -21,4 +21,8 @@ public class test {
         System.out.print(">> ");
         System.out.println("test");
     }
+
+    public static void Test(String[] args) {
+        System.out.println("Test");
+    }
 }
