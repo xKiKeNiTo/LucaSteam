e8f41414626f4702c1b7f14113eab0068d3d35ee
Kike <kike.verac@gmail.com>
2024-11-19 13:47:43 +0100

    Test2

--- src/menu/test.java
+++ src/menu/test.java
@@ -19,5 +19,6 @@ public class test {
         System.out.println("************************************************");
         System.out.println(" ");
         System.out.print(">> ");
+        System.out.println("test");
     }
 }
